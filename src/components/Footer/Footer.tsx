a0c4cab03b00917d7f1188150f94f9dd882e57aa
cmckinney-59 <chazdmckinney@gmail.com>
2025-05-28 08:51:04 -0600

    added facebook and instagram links to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,16 +4,32 @@ import "./Footer.css";
 
 export default function Footer(): JSX.Element {
   return (
-    <header className="App-footer">
+    <footer className="App-footer">
       <div className="footer-content">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-Footer-Text">FOOTER</h1>
+        <h1 className="App-Footer-Text">The Baybayin Project</h1>
       </div>
-      <div className="footer-links-containter">
-        <li>Facebook</li>
-        <li>Instagram</li>
-        <li>Email</li>
-      </div>
-    </header>
+      <ul className="footer-links-container">
+        <li>
+          <a
+            href="https://www.facebook.com/profile.php?id=61558642794586"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Facebook
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/the_baybayin_project/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
+        </li>
+        <a>Contact us: the.baybayin.project@gmail.com</a>
+      </ul>
+    </footer>
   );
 }
