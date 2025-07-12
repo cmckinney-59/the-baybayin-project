6018e3cfd89b74522c516e847426f1f025bec52a
cmckinney-59 <chazdmckinney@gmail.com>
2025-07-12 15:46:39 -0600

    added nav buttons

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,11 +1,26 @@
 import type { JSX } from "react";
 import "./Navigation.css";
+import { Link } from "react-router-dom";
 
 export default function Navigation(): JSX.Element {
   return (
     <nav className="App-Navigation">
       <div className="Navigation-content">
         <h1 className="App-Navigation-Text">Navigation</h1>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/baybayin">Baybayin</Link>
+          </li>
+          <li>
+            <Link to="/deseret">Deseret</Link>
+          </li>
+          <li>
+            <Link to="/aurbesh">Aurbesh</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
