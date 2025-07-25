8c3bced246ab99d066471099771edf3cae24f57d
cmckinney-59 <chazdmckinney@gmail.com>
2025-07-24 22:42:39 -0600

    commented some stuff out

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,20 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
-import Navigation from "./components/Navigation/Navigation";
+// import Navigation from "./components/Navigation/Navigation";
 import NavigationDropdown from "./components/NavigationDropdown/NavigationDropdown";
-// import Footer from "./components/Footer/Footer";
+import Footer from "./components/Footer/Footer";
 
 function RootLayout() {
   return (
     <>
       <div className="App">
         <Header />
-        <Navigation />
+        {/* <Navigation /> */}
         <NavigationDropdown />
         <main className="App-content">
           <Outlet />
         </main>
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </>
   );
