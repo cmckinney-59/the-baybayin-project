cdb0a872812258b0ada33836fb510a84a5546b3e
cmckinney-59 <chazdmckinney@gmail.com>
2025-09-04 08:25:54 -0600

    updated header open close icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -18,9 +18,32 @@ export default function Header({
         aria-label="Toggle navigation"
         onClick={onToggleSidebar}
       >
-        <span />
-        <span />
-        <span />
+        {isSidebarOpen ? (
+          <svg
+            className="icon close-icon"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            aria-hidden="true"
+            focusable="false"
+          >
+            <line x1="5" y1="5" x2="19" y2="19" />
+            <line x1="19" y1="5" x2="5" y2="19" />
+          </svg>
+        ) : (
+          <svg
+            className="icon hamburger-icon"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            aria-hidden="true"
+            focusable="false"
+          >
+            <line x1="4" y1="6" x2="20" y2="6" />
+            <line x1="4" y1="12" x2="20" y2="12" />
+            <line x1="4" y1="18" x2="20" y2="18" />
+          </svg>
+        )}
       </button>
       <div className="header-content">
         <img src={logo} className="App-logo" alt="logo" />
