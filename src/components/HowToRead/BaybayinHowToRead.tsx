bd3745566c465476e28ad878051dc1d541961b11
cmckinney-59 <chazdmckinney@gmail.com>
2025-09-06 15:07:47 -0600

    started baybayin how to read

--- src/components/HowToRead/BaybayinHowToRead.tsx
+++ src/components/HowToRead/BaybayinHowToRead.tsx
@@ -1,11 +1,24 @@
-import baybayinAlphabet from "../../assets/images/How to read/Baybayin/alphabet.jpg";
+import baybayinAlphabetImage from "../../assets/images/How to read/Baybayin/alphabet.jpg";
+import amaImage from "../../assets/images/How to read/Baybayin/ama.png";
+import maaariImage
 
 export default function BaybayinHowToRead() {
   return (
     <>
       <h2>How to read Baybayin</h2>
-      <img src={baybayinAlphabet} alt="baybayin-alphabet-image" />
-      <p>Above is the baybayin alphabet</p>
+      <p>
+        Below is a chart (article linked below) of the symbols used in the
+        transliteration I made. This form of Baybayin is called “Modified
+        Baybayin” and is the most popular form of the script.
+      </p>
+      <img src={baybayinAlphabetImage} alt="baybayin-alphabet-image" />
+      <p>
+        The first row of symbols above are vowels. These are typically used when
+        a word starts with a vowel, like “ama”, or there are two consecutive
+        vowels, like “maaari”. See below examples:
+      </p>
+      <img src={amaImage} alt="ama-image" />
+      <img src={amaImage} alt="ama-image" />
     </>
   );
 }
