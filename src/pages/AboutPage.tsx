6d35b63c3d293d011df8667c390c29fab580fa54
cmckinney-59 <chazdmckinney@gmail.com>
2025-09-17 15:27:17 -0600

    edited about page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -3,74 +3,73 @@ export default function AboutPage() {
     <>
       <h1>About</h1>
       <p>
-        How it began In the summer of 2013, I opened my mission call for The
-        Church of Jesus Christ of Latter-day Saint in the house that I grew up
-        in, in Utah. I was called to serve in The Philippines, Tagalog speaking.
-        This came as a huge surprise to me and my family. We had assumed that I
-        was going stateside due to dietary restrictions. On top of that, the
-        only thing that I really knew about the Philippines was that it is an
-        archipelago in Asia. I had no idea how much of a positive effect the
-        people and culture of The Philippines would have on my life. January 1,
-        2014 I boarded a flight to the Philippines to attend the Missionary
-        Training Center. The days were long and grueling and by the end of my
-        time in the MTC I had made some great friends. Upon leaving, I felt like
-        I was at the bottom of my class when it came to speaking Tagalog.
-        Luckily, I had a phenomenal trainer who was patient with my language
-        learning. He taught me the importance of diligently studying the
-        language every single day. I went about six months not feeling
-        comfortable speaking Tagalog. Then I got called to train a new
-        missionary. It was sink or swim because now I had to lead contacting and
-        lessons. I was again fortunate to be blessed with another supportive and
-        patient companion as I learned the language. Jaryl Miguel, who is native
-        to the Philippines. We discovered the Baybayin (also known as Alibata)
-        script in a Tagalog dictionary and started to learn it. Soon enough we
-        were able to effortlessly write notes back and forth in this ancient
-        script. (Elder (Jaryl) Miguel and I in the Philippines Quezon City North
-        Mission circa 2014) By the end of my mission, I was speaking Tagalog
-        just as well as the friends I made in the MTC. Even Filipinos would
-        mistake me for a kababayan (countrymen) if they heard me speak without
-        seeing my face. I feel like I was diligent in my study therefore I “won
-        the prize” (Mosiah 4:27). That prize was the gift of tongues. Soon after
-        returning home, I started working in the church in a call center for The
-        Philippines. This position gave me the opportunity to keep my language
-        skills while also continuing to contribute to the church growth in The
-        Philippines and work with some wonderful Filipinos. In 2018 I enrolled
-        at BYU-Hawaii. This was a great opportunity for me to continue my
-        education. It also allowed me to continue speaking Tagalog due to all of
-        the Filipinos attending school there. To my surprise, my former mission
-        companion, Elder Miguel, lived in the dorm just a few doors down. There
-        I met another great friend of mine, EJ Pelera. He was from Baguio, which
-        is a city just a few hours north of my mission. It was amazing to
-        interact with and form deeper connections with both EJ and Jaryl at
-        BYUH. (EJ Pelera and I at BYU-Hawaii circa 2019) During my last semester
-        I took a class where we used Excel to deconstruct and analyze Jacob
-        chapter 5. The principles that I learned in this class would build the
-        foundational skills required to begin my project of transcribing "Ang
-        Aklat Ni Mormon" (The Book of Mormon) into the book you hold now. After
-        completing a Bachelor's degree from BYUH in late 2019, I returned home.
-        During 2020 April General Conference the thought came to me that I may
-        be able to transcribe “Ang Aklat Ni Mormon” from its modern script into
-        Baybayin.This is where my journey of transcription began. While
-        listening to one session of conference I worked on developing a proof of
-        concept. Before the end of that session I had completed a full
-        transcription of First Nephi chapter 1 into Baybain. Over the course of
-        the following months, I was able to transcribe, review, edit and format
-        most of the “Ang Aklat ni Mormon”, but I was still missing a second and
-        third look over. That is when I enlisted the help of my friends Jaryl
-        and EJ. I sent them over a list of the unique words (about 15,700 words)
-        to review and a couple of months later they came back to me with
-        meaningful feedback and changes. It took me nearly two years to fully
-        implement their changes due to significant life changes that happened. I
-        moved to a new country, married the love of my life, took on two
-        wonderful stepdaughters, started a pregnancy and was given more
-        responsibility at work. But, “sa awa ng Diyos” (through the grace of
-        God) I was able to finish the updating and editing by the end April
-        2023. Overall, I have had a great time working on this project and
-        collaborating with two great friends of mine. I have learned so much
-        from the Filipino community both within the Philippines and worldwide. I
-        am excited and hopeful that this transcription of the “Ang Aklat ni
-        Mormon” will be a positive contribution to the Filipino literature and
-        culture. Maraming salamat sa inyong lahat! -Chaz
+        Hi, my name is Chaz. I am a software engineer and a husband and father.
+        I am also the founder of The Baybayin Project. I was born and raised in
+        Utah. I currently live in Utah with my wife and three children.
+      </p>
+      <h2>How it began</h2>
+      <p>
+        In the summer of 2013, I opened my mission call for The Church of Jesus
+        Christ of Latter-day Saint. I was called to serve in The Philippines for
+        two years and was to learn Tagalog. This came as a huge surprise to me
+        and my family. At the time the only thing that I really knew about the
+        Philippines was that it is an archipelago in Asia. I had no idea how
+        much of a positive effect the people and culture of The Philippines
+        would have on my life. On January 1, 2014 I boarded a flight to the
+        Philippines to attend the Missionary Training Center. There I would
+        study and learn Tagalog. The days were long and grueling. Upon leaving,
+        I felt like I was at the bottom of my class when it came to speaking
+        Tagalog. Luckily, I had a phenomenal trainer in the field who was
+        patient with my language learning. He taught me the importance of
+        diligently studying the language every single day. Over the course of my
+        mission that daily habit compounded and turned me into a fluent speaker
+        of Tagalog. At some point I discovered the Baybayin (also known as
+        Alibata) script in a Tagalog dictionary and started to learn it. Soon
+        enough I was able to effortlessly write notes back and forth with
+        friends of mine who also learned the baybayin script.
+      </p>
+      <p>
+        Soon after returning home, I started working for The Church of Jesus
+        Christ of Latter-day Saints in a call center for The Philippines. This
+        position gave me the opportunity to keep my language skills while also
+        continuing to work with some amazing Filipinos.
+      </p>
+      <p>
+        In 2018 I enrolled at BYU-Hawaii. This was not only a great opportunity
+        for me to continue my education, but it also allowed me to continue
+        speaking Tagalog due to all of the Filipinos that attended school there.
+        During my last semester I took a class where we used Excel to
+        deconstruct and analyze a chapter of a book. The principles that I
+        learned in this class would build the foundational skills required to
+        begin my first project, transcribing "Ang Aklat Ni Mormon" (The Book of
+        Mormon).
+      </p>
+      <p>
+        After completing a Bachelor's degree from BYUH in 2019, I returned home.
+        In April of 2020, the thought came to me that I may be able to
+        transcribe “Ang Aklat Ni Mormon” into Baybayin. I soon had a proof of
+        concept for the first chapter of ANM. Over the course of the following
+        months, I was able to transcribe, review, edit and format most of the
+        “Ang Aklat ni Mormon”. I sent two of my friends over a list of the
+        unique words (about 15,700 words) to review and a couple of months later
+        they came back to me with meaningful feedback and changes.
+      </p>
+      <p>
+        It took me nearly two years to fully implement their changes due to
+        significant life changes that happened. I moved to a new country,
+        married the love of my life, took on two wonderful stepdaughters,
+        started a pregnancy and was given more responsibility at work. But, “sa
+        awa ng Diyos” (through the grace of God) I was able to finish the
+        updating and editing the first draft by April 2023.
+      </p>
+      <p>
+        We currently have several projects in the works. For a full list of
+        projects, please visit the <a href="/projects">Projects</a> page.
+      </p>
+      <p>
+        Overall, I have had a great time working on these projects. I have
+        learned so much from the Filipino community both within the Philippines
+        and worldwide. Maraming salamat sa inyong lahat! -Chaz
       </p>
     </>
   );
