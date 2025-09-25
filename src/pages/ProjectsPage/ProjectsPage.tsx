565eb3b4ca9647acbe3fd3db390678c48f9a6cb1
cmckinney-59 <chazdmckinney@gmail.com>
2025-09-25 12:20:28 -0600

    updated projects page

--- src/pages/ProjectsPage/ProjectsPage.tsx
+++ src/pages/ProjectsPage/ProjectsPage.tsx
@@ -17,15 +17,16 @@ export default function ProjectsPage() {
         <p>Here are some of our projects available for download:</p>
         <div className="projectDownloads">
           <div className="downloadItem">
-            <h3>Ang Buhay Na Cristo</h3>
+            <h3>How to Read Baybayin</h3>
             <button
-              onClick={() => handleDownload("17409_tgl.pdf")}
+              onClick={() => handleDownload("HowToReadBaybayin.pdf")}
               className="downloadButton"
             >
               Download PDF
             </button>
           </div>
         </div>
+        <h2>More to come...</h2>
       </div>
     </div>
   );
