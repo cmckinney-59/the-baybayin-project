15c657cbb8d492d016944d96007f053e1c8bfff9
cmckinney-59 <chazdmckinney@gmail.com>
2025-10-02 14:54:52 -0600

    updated how to read section

--- src/components/HowToRead/BaybayinHTR/BaybayinHowToRead/BaybayinHowToRead.tsx
+++ src/components/HowToRead/BaybayinHTR/BaybayinHowToRead/BaybayinHowToRead.tsx
@@ -190,29 +190,369 @@ export default function BaybayinHowToRead() {
         to produce a similar sound. Here is a table of the most notable borrowed
         sounds and how they are expressed in Baybayin:
       </p>
+
       <BaybayinTableBorrowed />
       <h2>Examples</h2>
       <p>
-        The first three symbols in the table represent vowels. Tagalog vowels
-        originally only consisted of A, I and O. Then when borrowed words were
-        introduced from Spanish and English, E and U were added and utilized the
-        same symbol as I and O respectively. These are typically used when a
-        word starts with a vowel, like “ama”, or when there are two consecutive
-        vowels, like “maaari”. See examples below:
+        Some are a combination of symbols used to mimic certain sounds like siy
+        (sh) and f (ph). See examples below:
       </p>
       <table className="baybayin-example">
         <thead>
           <tr>
-            <th className="baybayin-letter">am</th>
-            <th className="baybayin-letter">a</th>
-            <th className="baybayin-letter">m</th>
+            <th className="baybayin-letter">siym+pu</th>
+            <th className="baybayin-letter">siy</th>
+            <th className="baybayin-letter">m+</th>
+            <th className="baybayin-letter">pu</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>ama</td>
+            <td>shampoo</td>
+            <td>sha</td>
+            <td>m</td>
+            <td>poo</td>
+          </tr>
+        </tbody>
+      </table>
+      <table className="baybayin-example">
+        <thead>
+          <tr>
+            <th className="baybayin-letter">filipin+s+</th>
+            <th className="baybayin-letter">fi</th>
+            <th className="baybayin-letter">li</th>
+            <th className="baybayin-letter">pi</th>
+            <th className="baybayin-letter">n+</th>
+            <th className="baybayin-letter">s+</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Philippines</td>
+            <td>Phi</td>
+            <td>li</td>
+            <td>p(p)i</td>
+            <td>n(e)</td>
+            <td>s</td>
+          </tr>
+        </tbody>
+      </table>
+      <p>
+        Some letters are borrowed from both Spanish and English but make
+        different sounds according to the context. These are mainly j and qu.
+        See examples below:
+      </p>
+      <table className="baybayin-example">
+        <thead>
+          <tr>
+            <th className="baybayin-letter">kuwes+tiOn+</th>
+            <th className="baybayin-letter">kuwe</th>
+            <th className="baybayin-letter">s+</th>
+            <th className="baybayin-letter">ti</th>
+            <th className="baybayin-letter">O</th>
+            <th className="baybayin-letter">n+</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>question</td>
+            <td>que</td>
+            <td>s</td>
+            <td>ti</td>
+            <td>o</td>
+            <td>n</td>
+          </tr>
+        </tbody>
+      </table>
+      <table className="baybayin-example">
+        <thead>
+          <tr>
+            <th className="baybayin-letter">kezon+</th>
+            <th className="baybayin-letter">ke</th>
+            <th className="baybayin-letter">zo</th>
+            <th className="baybayin-letter">n+</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Quezon</td>
+            <td>Que</td>
+            <td>zo</td>
+            <td>n</td>
+          </tr>
+        </tbody>
+      </table>
+      <table className="baybayin-example">
+        <thead>
+          <tr>
+            <th className="baybayin-letter">diyesus+</th>
+            <th className="baybayin-letter">diye</th>
+            <th className="baybayin-letter">su</th>
+            <th className="baybayin-letter">s+</th>
+          </tr>
+        </thead>
+        <thead>
+          <tr>
+            <th className="baybayin-letter">hesus+</th>
+            <th className="baybayin-letter">he</th>
+            <th className="baybayin-letter">su</th>
+            <th className="baybayin-letter">s+</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Jesus</td>
+            <td>Je</td>
+            <td>su</td>
+            <td>s</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <p>
+        The letter C contributes to forming multiple different sounds in various
+        borrowed words. It can be expressed having an ‘s’, ‘k’ or ‘ch’ sound.
+        See examples below:
+      </p>
+      <table className="baybayin-example">
+        <thead>
+          <tr>
+            <th className="baybayin-letter">sen+t+ro</th>
+            <th className="baybayin-letter">se</th>
+            <th className="baybayin-letter">n+</th>
+            <th className="baybayin-letter">t+</th>
+            <th className="baybayin-letter">ro</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>centro</td>
+            <td>ce</td>
+            <td>n</td>
+            <td>t</td>
+            <td>ro</td>
+          </tr>
+        </tbody>
+      </table>
+      <table className="baybayin-example">
+        <thead>
+          <tr>
+            <th className="baybayin-letter">sek+reto</th>
+            <th className="baybayin-letter">se</th>
+            <th className="baybayin-letter">k+</th>
+            <th className="baybayin-letter">re</th>
+            <th className="baybayin-letter">to</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>secreto</td>
+            <td>se</td>
+            <td>k</td>
+            <td>re</td>
+            <td>to</td>
+          </tr>
+        </tbody>
+      </table>
+      <table className="baybayin-example">
+        <thead>
+          <tr>
+            <th className="baybayin-letter">an+tiyen+t+</th>
+            <th className="baybayin-letter">A</th>
+            <th className="baybayin-letter">n+</th>
+            <th className="baybayin-letter">tiye</th>
+            <th className="baybayin-letter">n+</th>
+            <th className="baybayin-letter">t+</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>ancient</td>
             <td>a</td>
-            <td>ma</td>
+            <td>n</td>
+            <td>cie</td>
+            <td>n</td>
+            <td>t</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <p>
+        When Ch is used it commonly is expressed as ‘ch’ or ‘k’. See examples
+        below:
+      </p>
+      <table className="baybayin-example">
+        <thead>
+          <tr>
+            <th className="baybayin-letter">tiyur+tiy+</th>
+            <th className="baybayin-letter">tiyu</th>
+            <th className="baybayin-letter">r+</th>
+            <th className="baybayin-letter">tiy+</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>church</td>
+            <td>chu</td>
+            <td>r</td>
+            <td>ch</td>
+          </tr>
+        </tbody>
+      </table>
+      <table className="baybayin-example">
+        <thead>
+          <tr>
+            <th className="baybayin-letter">meknik+</th>
+            <th className="baybayin-letter">me</th>
+            <th className="baybayin-letter">k</th>
+            <th className="baybayin-letter">ni</th>
+            <th className="baybayin-letter">k+</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>mechanic</td>
+            <td>me</td>
+            <td>cha</td>
+            <td>ni</td>
+            <td>c</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <p>
+        E and O also originally didn’t exist and are expressed in the same way
+        as I and U respectively. See examples below:
+      </p>
+      <table className="baybayin-example">
+        <thead>
+          <tr>
+            <th className="baybayin-letter">hesus+</th>
+            <th className="baybayin-letter">he</th>
+            <th className="baybayin-letter">su</th>
+            <th className="baybayin-letter">s+</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Jesus</td>
+            <td>Je</td>
+            <td>su</td>
+            <td>s</td>
+          </tr>
+        </tbody>
+      </table>
+      <table className="baybayin-example">
+        <thead>
+          <tr>
+            <th className="baybayin-letter">Ehip+to</th>
+            <th className="baybayin-letter">E</th>
+            <th className="baybayin-letter">hi</th>
+            <th className="baybayin-letter">p+</th>
+            <th className="baybayin-letter">to</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Egipto</td>
+            <td>E</td>
+            <td>gi</td>
+            <td>p</td>
+            <td>to</td>
+          </tr>
+        </tbody>
+      </table>
+      <table className="baybayin-example">
+        <thead>
+          <tr>
+            <th className="baybayin-letter">Uso</th>
+            <th className="baybayin-letter">U</th>
+            <th className="baybayin-letter">so</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>uso</td>
+            <td>u</td>
+            <td>so</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <p>
+        The final borrowed consonants are V, Ph, P and Z. See examples below:
+      </p>
+      <table className="baybayin-example">
+        <thead>
+          <tr>
+            <th className="baybayin-letter">bk</th>
+            <th className="baybayin-letter">b</th>
+            <th className="baybayin-letter">k</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>vaka (baka)</td>
+            <td>va</td>
+            <td>ka</td>
+          </tr>
+        </tbody>
+      </table>
+      <table className="baybayin-example">
+        <thead>
+          <tr>
+            <th className="baybayin-letter">filipin+s+</th>
+            <th className="baybayin-letter">fi</th>
+            <th className="baybayin-letter">li</th>
+            <th className="baybayin-letter">pi</th>
+            <th className="baybayin-letter">n+</th>
+            <th className="baybayin-letter">s+</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Philippines</td>
+            <td>Phi</td>
+            <td>li</td>
+            <td>p(p)i</td>
+            <td>n(e)</td>
+            <td>s</td>
+          </tr>
+        </tbody>
+      </table>
+      <table className="baybayin-example">
+        <thead>
+          <tr>
+            <th className="baybayin-letter">p+lno</th>
+            <th className="baybayin-letter">p+</th>
+            <th className="baybayin-letter">l</th>
+            <th className="baybayin-letter">no</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>plano</td>
+            <td>p</td>
+            <td>la</td>
+            <td>no</td>
+          </tr>
+        </tbody>
+      </table>
+      <table className="baybayin-example">
+        <thead>
+          <tr>
+            <th className="baybayin-letter">zeb+r</th>
+            <th className="baybayin-letter">ze</th>
+            <th className="baybayin-letter">b+</th>
+            <th className="baybayin-letter">r</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>zebra</td>
+            <td>ze</td>
+            <td>b</td>
+            <td>ra</td>
           </tr>
         </tbody>
       </table>
