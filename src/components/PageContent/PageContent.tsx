42be9e829c513e18dbb2b733a8576b9dda0d773d
cmckinney-59 <chazdmckinney@gmail.com>
2025-10-05 22:58:24 -0600

    removed old code

--- src/components/PageContent/PageContent.tsx
+++ src/components/PageContent/PageContent.tsx
@@ -3,22 +3,17 @@ import "./PageContent.css";
 
 import Transliterator from "../Transliterator/Transliterator.tsx";
 import Transliterator2 from "../Transliterator2/Transliterator2.tsx";
-// import Description from "../Description/Description.tsx";
-import { PAGES } from "../../pages.tsx";
 import TransliteratorLite from "../TransliteratorLite/TransliteratorLite.tsx";
 
 interface PageContentProps {
-  selectedAlphabet: string;
   currentPage: string;
+  title: string;
 }
 
 export default function PageContent({
-  selectedAlphabet,
   currentPage,
+  title,
 }: PageContentProps): JSX.Element {
-  const pageData = PAGES[selectedAlphabet] || {};
-  const { title } = pageData;
-
   return (
     <main className="page-content">
       {title && title !== "Home" && <Transliterator title={title} />}
@@ -27,7 +22,6 @@ export default function PageContent({
       ) : (
         <Transliterator2 title={title} />
       )}
-      {/* <Description image={image} whatIs={whatIs} description={description} /> */}
     </main>
   );
 }
