6bbf0f8236762e6d7c54ffea78f50c82d9b9a502
cmckinney-59 <chazdmckinney@gmail.com>
2025-10-09 20:40:47 -0600

    updated open close to be actual icons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,12 @@
 import type { JSX } from "react";
 // import logo from "../../assets/images/logo.png";
 import "./Header.css";
-import { AiOutlineMoon, AiOutlineSun } from "react-icons/ai";
+import {
+  AiOutlineClose,
+  AiOutlineMenu,
+  AiOutlineMoon,
+  AiOutlineSun,
+} from "react-icons/ai";
 
 type HeaderProps = {
   onToggleSidebar: () => void;
@@ -24,30 +29,9 @@ export default function Header({
         onClick={onToggleSidebar}
       >
         {isSidebarOpen ? (
-          <svg
-            className="icon close-icon"
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-            aria-hidden="true"
-            focusable="false"
-          >
-            <line x1="5" y1="5" x2="19" y2="19" />
-            <line x1="19" y1="5" x2="5" y2="19" />
-          </svg>
+          <AiOutlineClose className="icon close-icon" />
         ) : (
-          <svg
-            className="icon hamburger-icon"
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-            aria-hidden="true"
-            focusable="false"
-          >
-            <line x1="4" y1="6" x2="20" y2="6" />
-            <line x1="4" y1="12" x2="20" y2="12" />
-            <line x1="4" y1="18" x2="20" y2="18" />
-          </svg>
+          <AiOutlineMenu className="icon hamburger-icon" />
         )}
       </button>
       <div className="header-content">
