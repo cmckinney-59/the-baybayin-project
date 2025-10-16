5b1f8d5b54f20709d885a45dbc510800d5874352
cmckinney-59 <chazdmckinney@gmail.com>
2025-10-16 15:29:38 -0600

    removed some comments

--- src/utils/DownloadFont.ts
+++ src/utils/DownloadFont.ts
@@ -3,21 +3,10 @@
  * @param fileName - The name of the file to download
  */
 export default function downloadFont(fileName: string) {
-  // Create a temporary link element
   const link = document.createElement("a");
-
-  // Set the href to the file in the public directory with correct base path
   link.href = `/the-baybayin-project/${fileName}`;
-
-  // Set the download attribute with the desired filename
   link.download = fileName;
-
-  // Add the link to the DOM temporarily
   document.body.appendChild(link);
-
-  // Trigger the download
   link.click();
-
-  // Remove the link from the DOM
   document.body.removeChild(link);
 }
