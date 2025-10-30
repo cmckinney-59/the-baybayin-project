1f3f08e6a3f4d79fe71cf76d826c83f1180d68b5
cmckinney-59 <chazdmckinney@gmail.com>
2025-10-29 21:46:51 -0600

    configured aurebesh to work properly

--- src/utils/TextProcessors/AurebeshTextProcessor.ts
+++ src/utils/TextProcessors/AurebeshTextProcessor.ts
@@ -1,4 +1,12 @@
 export default function processAurebeshText(text: string): string {
   let transliteratedText = text;
+  transliteratedText = transliteratedText.replace(/ch/g, "ç");
+  transliteratedText = transliteratedText.replace(/ae/g, "æ");
+  transliteratedText = transliteratedText.replace(/eo/g, "ë");
+  transliteratedText = transliteratedText.replace(/kh/g, "þ");
+  transliteratedText = transliteratedText.replace(/ng/g, "ñ");
+  transliteratedText = transliteratedText.replace(/oo/g, "ø");
+  transliteratedText = transliteratedText.replace(/sh/g, "ß");
+  transliteratedText = transliteratedText.replace(/th/g, "ð");
   return transliteratedText;
 }
