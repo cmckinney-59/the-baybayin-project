dd797ae0ff9963ddd65b5bd0e886606a57af2142
cmckinney-59 <chazdmckinney@gmail.com>
2025-11-04 21:38:30 -0700

    updated app to use alphabetprovider

--- src/pages/TransliteratorPages/AurebeshPage.tsx
+++ src/pages/TransliteratorPages/AurebeshPage.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import TransliteratorLiteAurebesh from "../../components/TransliteratorLiteAurebesh/TransliteratorLiteAurebesh";
+import { useAlphabet } from "../../contexts/AlphabetContext";
 
 export default function AurebeshPage() {
+  const { setCurrentAlphabet } = useAlphabet();
+
+  useEffect(() => {
+    setCurrentAlphabet("Aurebesh");
+  }, [setCurrentAlphabet]);
+
   return (
     <>
       <TransliteratorLiteAurebesh title="Aurebesh" />
--- src/pages/TransliteratorPages/BaybayinPage.tsx
+++ src/pages/TransliteratorPages/BaybayinPage.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import Transliterator3 from "../../components/Transliterator3/Transliterator3";
+import { useAlphabet } from "../../contexts/AlphabetContext";
 
 export default function BaybayinPage() {
+  const { setCurrentAlphabet } = useAlphabet();
+
+  useEffect(() => {
+    setCurrentAlphabet("Baybayin");
+  }, [setCurrentAlphabet]);
+
   return (
     <>
       <Transliterator3 title="Baybayin" />
