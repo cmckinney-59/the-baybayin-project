4adea816ea0800cc5ad3e73e7bb67a28c6a340b4
cmckinney-59 <chazdmckinney@gmail.com>
2025-12-08 22:41:46 -0700

    Updated what is Aurebesh

--- src/components/HowToRead/AurebeshHTR/WhatIsAurebesh/WhatIsAurebesh.tsx
+++ src/components/HowToRead/AurebeshHTR/WhatIsAurebesh/WhatIsAurebesh.tsx
@@ -4,18 +4,7 @@ export default function WhatIsAurebesh() {
   return (
     <CollapsibleSection title="What Is Aurebesh" defaultExpanded={false}>
       <p>Aurebesh is the alphabet used in the Star Wars franchise.</p>
-      <p>
-        To explore Aurebesh further, read the "How To Read" section, or download
-        our comprehensive{" "}
-        <a
-          href="/src/assets/projects/AurebeshHowToReadV2.pdf"
-          download="HowToReadAurebesh.pdf"
-          className="link"
-        >
-          "How To Read Aurebesh"
-        </a>{" "}
-        PDF guide.
-      </p>
+      <p>To explore Aurebesh further, read the "How To Read" section.</p>
     </CollapsibleSection>
   );
 }
