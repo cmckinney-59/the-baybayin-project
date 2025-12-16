5643a046da193828141267e5c5d505935e9a6e1c
cmckinney-59 <chazdmckinney@gmail.com>
2025-12-16 15:17:58 -0700

    added a back button

--- src/pages/TransliteratorPages/AurebeshPage.tsx
+++ src/pages/TransliteratorPages/AurebeshPage.tsx
@@ -1,4 +1,6 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 import TransliteratorLiteAurebesh from "../../components/TransliteratorLiteAurebesh/TransliteratorLiteAurebesh";
 import { useAlphabet } from "../../contexts/AlphabetContext";
 import { WordsDictionaryProvider } from "../../contexts/WordsDictionaryContext.tsx";
@@ -8,13 +10,21 @@ import HowToUse from "../../components/HowToRead/HowToUse";
 
 export default function AurebeshPage() {
   const { setCurrentAlphabet } = useAlphabet();
+  const navigate = useNavigate();
 
   useEffect(() => {
     setCurrentAlphabet("Aurebesh");
   }, [setCurrentAlphabet]);
 
+  const handleBackClick = () => {
+    navigate("/transliterator/baybayin");
+  };
+
   return (
     <WordsDictionaryProvider>
+      <button className="back-button" onClick={handleBackClick}>
+        <AiOutlineArrowLeft /> Back to Baybayin
+      </button>
       <TransliteratorLiteAurebesh title="Aurebesh" />
       <HowToUse />
       <WhatIsAurebesh />
