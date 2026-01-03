7bf41193a6ab19f32c64331589cc8852982e3154
cmckinney-59 <chazdmckinney@gmail.com>
2026-01-03 14:22:58 -0700

    added settings option component

---
+++ src/components/SettingsOption/SettingsOption.tsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import "./SettingsPage.css";
+
+export default function SettingsPage() {
+  const [showExperimentalFeatures, setShowExperimentalFeatures] = useState(
+    () => {
+      const saved = localStorage.getItem("showExperimentalFeatures");
+      return saved ? JSON.parse(saved) : false;
+    }
+  );
+
+  useEffect(() => {
+    localStorage.setItem(
+      "showExperimentalFeatures",
+      JSON.stringify(showExperimentalFeatures)
+    );
+  }, [showExperimentalFeatures]);
+
+  const toggleExperimentalFeatures = () => {
+    setShowExperimentalFeatures((prev: boolean) => !prev);
+  };
+
+  return (
+    <div className="setting-item">
+      <h2>Show Experimental Features</h2>
+      <label className="toggle-switch">
+        <input
+          type="checkbox"
+          checked={showExperimentalFeatures}
+          onChange={toggleExperimentalFeatures}
+        />
+        <span className="toggle-slider"></span>
+      </label>
+      <h2>Dark Mode</h2>
+      <label className="toggle-switch">
+        <input
+          type="checkbox"
+          // checked={isDarkMode}
+          // onChange={toggleDarkMode}
+        />
+        <span className="toggle-slider"></span>
+      </label>
+    </div>
+  );
+}
