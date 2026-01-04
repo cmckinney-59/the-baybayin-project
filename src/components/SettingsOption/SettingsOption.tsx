2c6019f2155ffc97b7e8a3b7256402e78f765c81
cmckinney-59 <chazdmckinney@gmail.com>
2026-01-03 21:51:11 -0700

    updated settings page

--- src/components/SettingsOption/SettingsOption.tsx
+++ src/components/SettingsOption/SettingsOption.tsx
@@ -1,5 +1,4 @@
-import { useState, useEffect } from "react";
-import "./SettingsPage.css";
+import "./SettingsOption.css";
 
 interface SettingsOptionProps {
   type: "checkbox" | "radio";
--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import "./SettingsPage.css";
 import SettingsOption from "../../components/SettingsOption/SettingsOption";
 
 export default function SettingsPage() {
