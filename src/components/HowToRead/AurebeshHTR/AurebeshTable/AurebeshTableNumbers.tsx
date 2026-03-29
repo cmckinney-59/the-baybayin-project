02e32c86cd431a1d5143c9191994eb97538068dc
cmckinney-59 <chazdmckinney@gmail.com>
2026-03-28 22:22:48 -0600

    added tech numerals

--- src/components/HowToRead/AurebeshHTR/AurebeshTable/AurebeshTableNumbers.tsx
+++ src/components/HowToRead/AurebeshHTR/AurebeshTable/AurebeshTableNumbers.tsx
@@ -2,53 +2,58 @@ export default function AurebeshTableNumbers() {
   return (
     <>
       <h2>Numbers</h2>
+      <p>
+        There are two types of numbers in used in Aurebesh. The first is styled
+        similar to arabic numbers, the second are numerals that somewhat
+        resemble braille.
+      </p>
       <table className="alphabet-table">
         <thead>
           <tr>
-            <th>Aurebesh Symbol</th>
-            <th>Latin Number</th>
+            <th>Arabic Styled</th>
+            <th>Tech Numeral</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td className="aurebesh-letter">0</td>
-            <td>0</td>
+            <td className="aurebesh-font-tech-numbers">0</td>
           </tr>
           <tr>
             <td className="aurebesh-letter">1</td>
-            <td>1</td>
+            <td className="aurebesh-font-tech-numbers">1</td>
           </tr>
           <tr>
             <td className="aurebesh-letter">2</td>
-            <td>2</td>
+            <td className="aurebesh-font-tech-numbers">2</td>
           </tr>
           <tr>
             <td className="aurebesh-letter">3</td>
-            <td>3</td>
+            <td className="aurebesh-font-tech-numbers">3</td>
           </tr>
           <tr>
             <td className="aurebesh-letter">4</td>
-            <td>4</td>
+            <td className="aurebesh-font-tech-numbers">4</td>
           </tr>
           <tr>
             <td className="aurebesh-letter">5</td>
-            <td>5</td>
+            <td className="aurebesh-font-tech-numbers">5</td>
           </tr>
           <tr>
             <td className="aurebesh-letter">6</td>
-            <td>6</td>
+            <td className="aurebesh-font-tech-numbers">6</td>
           </tr>
           <tr>
             <td className="aurebesh-letter">7</td>
-            <td>7</td>
+            <td className="aurebesh-font-tech-numbers">7</td>
           </tr>
           <tr>
             <td className="aurebesh-letter">8</td>
-            <td>8</td>
+            <td className="aurebesh-font-tech-numbers">8</td>
           </tr>
           <tr>
             <td className="aurebesh-letter">9</td>
-            <td>9</td>
+            <td className="aurebesh-font-tech-numbers">9</td>
           </tr>
         </tbody>
       </table>
