b00241c0c5da7eb8e9c872df20793d93737cb2c1
cmckinney-59 <chazdmckinney@gmail.com>
2026-03-30 14:51:05 -0600

    refactored projects row

--- src/pages/ProjectsPage/ProjectsRow.tsx
+++ src/pages/ProjectsPage/ProjectsRow.tsx
@@ -1,18 +1,19 @@
 import type { ProjectRow } from "./PROJECTS_DATA";
 
 export default function ProjectsRow(project: ProjectRow) {
-  if ("fileUrl" in project) {
-    const handleDownload = () => {
-      const link = document.createElement("a");
-      link.href = project.fileUrl;
-      link.download = project.fileName;
-      link.rel = "noopener";
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    };
+  let row: React.ReactNode = null;
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = "fileUrl" in project ? project.fileUrl : "";
+    link.download = "fileName" in project ? project.fileName : "";
+    link.rel = "noopener";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
-    return (
+  if ("fileUrl" in project) {
+    row = (
       <tr>
         <td>{project.name}</td>
         <td>
@@ -26,12 +27,14 @@ export default function ProjectsRow(project: ProjectRow) {
         </td>
       </tr>
     );
+  } else {
+    row = (
+      <tr>
+        <td>{project.name}</td>
+        <td>{project.status}</td>
+      </tr>
+    );
   }
 
-  return (
-    <tr>
-      <td>{project.name}</td>
-      <td>{project.status}</td>
-    </tr>
-  );
+  return row;
 }
