7d1dfd644904fadb107e554e997f2929a4ff6ac3
cmckinney-59 <chazdmckinney@gmail.com>
2026-03-31 14:42:54 -0600

    work on deseret transliterator

--- src/utils/TextProcessors/DeseretTextProcessor.ts
+++ src/utils/TextProcessors/DeseretTextProcessor.ts
@@ -1,15 +1,20 @@
-export default function processDeseretText(text: string): string {
+import { translateEnglishToDeseret } from "../../services/DeseretService.ts";
+
+export default async function processDeseretText(
+  text: string,
+): Promise<string> {
   let transliteratedText = text;
+  const deseretText = await translateEnglishToDeseret(text);
 
-  // OO
-  transliteratedText = transliteratedText.replace(/OO/g, "J");
-  transliteratedText = transliteratedText.replace(/Oo/g, "J");
-  transliteratedText = transliteratedText.replace(/oo/gi, "j");
+  // // OO
+  // transliteratedText = transliteratedText.replace(/OO/g, "J");
+  // transliteratedText = transliteratedText.replace(/Oo/g, "J");
+  // transliteratedText = transliteratedText.replace(/oo/gi, "j");
 
-  // TH
-  transliteratedText = transliteratedText.replace(/TH/g, "Ê");
-  transliteratedText = transliteratedText.replace(/Th/g, "Ê");
-  transliteratedText = transliteratedText.replace(/th/gi, "†");
+  // // TH
+  // transliteratedText = transliteratedText.replace(/TH/g, "Ê");
+  // transliteratedText = transliteratedText.replace(/Th/g, "Ê");
+  // transliteratedText = transliteratedText.replace(/th/gi, "†");
 
-  return transliteratedText;
+  return deseretText.deseret;
 }
