3ddbbf351fb92fbc6e00bc60f14dd1d237816a8b
cmckinney-59 <chazdmckinney@gmail.com>
2026-04-01 13:55:14 -0600

    updated deseret

--- src/utils/TextProcessors/DeseretTextProcessor.ts
+++ src/utils/TextProcessors/DeseretTextProcessor.ts
@@ -7,6 +7,13 @@ export default async function processDeseretText(
   text: string,
 ): Promise<string> {
   let transliteratedText = await translate(text, { format: "deseret" });
+  // th
+
   transliteratedText = transliteratedText.replace(/𐑄/g, "†");
+
+  // ee
+  transliteratedText = transliteratedText.replace(/𐐨/g, "‰");
+  transliteratedText = transliteratedText.replace(/𐐀/g, "é");
+
   return transliteratedText;
 }
