745b9005d8409628c6eade645fa82dc04cfb0f9f
cmckinney-59 <chazdmckinney@gmail.com>
2026-04-13 14:56:02 -0600

    added deseret data file

---
+++ src/data/DESERET_DATA.ts
@@ -0,0 +1,17 @@
+export type DeseretData = {
+  letter: string;
+  IPA: string;
+  sound: string;
+  name: string;
+  example: string;
+};
+
+export const DESERET_DATA: DeseretData[] = [
+  {
+    letter: "𐐀",
+    IPA: "/i/",
+    sound: "ee",
+    name: "Long I",
+    example: "<i>e</i>at",
+  },
+];
