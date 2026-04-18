d14627781767cc9a9c74a2b7996077eaf7018108
cmckinney-59 <chazdmckinney@gmail.com>
2026-04-17 23:40:42 -0600

    added aurebesh data file and updated aurebesh table

--- src/components/HowToRead/AurebeshHTR/AurebeshTable/AurebeshTable.tsx
+++ src/components/HowToRead/AurebeshHTR/AurebeshTable/AurebeshTable.tsx
@@ -1,3 +1,5 @@
+import { AUREBESH_DATA } from "../../../../data/AUREBESH_DATA";
+
 export default function AurebeshTable() {
   return (
     <>
@@ -11,136 +13,13 @@ export default function AurebeshTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className="aurebesh-letter">A</td>
-            <td>A</td>
-            <td>Aurek</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">B</td>
-            <td>B</td>
-            <td>Besh</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">C</td>
-            <td>C</td>
-            <td>Cresh</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">D</td>
-            <td>D</td>
-            <td>Dorn</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">E</td>
-            <td>E</td>
-            <td>Esk</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">F</td>
-            <td>F</td>
-            <td>Forn</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">G</td>
-            <td>G</td>
-            <td>Grek</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">H</td>
-            <td>H</td>
-            <td>Herf</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">I</td>
-            <td>I</td>
-            <td>Isk</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">J</td>
-            <td>J</td>
-            <td>Jenth</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">K</td>
-            <td>K</td>
-            <td>Krill</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">L</td>
-            <td>L</td>
-            <td>Leth</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">M</td>
-            <td>M</td>
-            <td>Mern</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">N</td>
-            <td>N</td>
-            <td>Nern</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">O</td>
-            <td>O</td>
-            <td>Osk</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">P</td>
-            <td>P</td>
-            <td>Peth</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">Q</td>
-            <td>Q</td>
-            <td>Qek</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">R</td>
-            <td>R</td>
-            <td>Resh</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">S</td>
-            <td>S</td>
-            <td>Senth</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">T</td>
-            <td>T</td>
-            <td>Trill</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">U</td>
-            <td>U</td>
-            <td>Usk</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">V</td>
-            <td>V</td>
-            <td>Vev</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">W</td>
-            <td>W</td>
-            <td>Wesk</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">X</td>
-            <td>X</td>
-            <td>Xesh</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">Y</td>
-            <td>Y</td>
-            <td>Yirt</td>
-          </tr>
-          <tr>
-            <td className="aurebesh-letter">Z</td>
-            <td>Z</td>
-            <td>Zerek</td>
-          </tr>
+          {AUREBESH_DATA.map((letter) => (
+            <tr key={letter.letter}>
+              <td className="aurebesh-letter">{letter.letter}</td>
+              <td>{letter.letter}</td>
+              <td>{letter.name}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </>
---
+++ src/data/AUREBESH_DATA.ts
@@ -0,0 +1,111 @@
+export type AurebeshData = {
+  letter: string;
+  name: string;
+};
+
+export const AUREBESH_DATA: AurebeshData[] = [
+  {
+    letter: "A",
+    name: "Aurek",
+  },
+  {
+    letter: "B",
+    name: "Besh",
+  },
+  {
+    letter: "C",
+    name: "Cresh",
+  },
+  {
+    letter: "D",
+    name: "Dorn",
+  },
+  {
+    letter: "E",
+    name: "Esk",
+  },
+  {
+    letter: "F",
+    name: "Forn",
+  },
+  {
+    letter: "G",
+    name: "Grek",
+  },
+  {
+    letter: "H",
+    name: "Herf",
+  },
+  {
+    letter: "I",
+    name: "Isk",
+  },
+  {
+    letter: "J",
+    name: "Jenth",
+  },
+  {
+    letter: "K",
+    name: "Krill",
+  },
+  {
+    letter: "L",
+    name: "Leth",
+  },
+  {
+    letter: "M",
+    name: "Mern",
+  },
+  {
+    letter: "N",
+    name: "Nern",
+  },
+  {
+    letter: "O",
+    name: "Osk",
+  },
+  {
+    letter: "P",
+    name: "Peth",
+  },
+  {
+    letter: "Q",
+    name: "Qek",
+  },
+  {
+    letter: "R",
+    name: "Resh",
+  },
+  {
+    letter: "S",
+    name: "Senth",
+  },
+  {
+    letter: "T",
+    name: "Trill",
+  },
+  {
+    letter: "U",
+    name: "Usk",
+  },
+  {
+    letter: "V",
+    name: "Vev",
+  },
+  {
+    letter: "W",
+    name: "Wesk",
+  },
+  {
+    letter: "X",
+    name: "Xesh",
+  },
+  {
+    letter: "Y",
+    name: "Yirt",
+  },
+  {
+    letter: "Z",
+    name: "Zerek",
+  },
+];
