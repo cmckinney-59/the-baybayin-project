8a30730127a016e57bc6566a7e2f7da078c4bb03
cmckinney-59 <chazdmckinney@gmail.com>
2026-04-24 08:25:59 -0600

    updated baybayin table

--- src/components/HowToRead/BaybayinHTR/BaybayinTable/BaybayinTable.tsx
+++ src/components/HowToRead/BaybayinHTR/BaybayinTable/BaybayinTable.tsx
@@ -1,3 +1,5 @@
+import { BAYBAYIN_DATA } from "../../../../data/BAYBAYIN_DATA";
+
 export default function BaybayinTable() {
   return (
     <>
@@ -11,106 +13,13 @@ export default function BaybayinTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className="baybayin-letter">A</td>
-            <td>A</td>
-            <td>/a/</td>
-          </tr>
-          <tr>
-            <td className="baybayin-letter">I</td>
-            <td>E / I</td>
-            <td>/e/ /i/</td>
-          </tr>
-          <tr>
-            <td className="baybayin-letter">O</td>
-            <td>O / U</td>
-            <td>/o/ /u/</td>
-          </tr>
-          <tr>
-            <td className="baybayin-letter">k</td>
-            <td>Ca / Ka</td>
-            <td>/ca/ /ka/</td>
-          </tr>
-          <tr>
-            <td className="baybayin-letter">b</td>
-            <td>Ba / Va</td>
-            <td>/ba/ /va/</td>
-          </tr>
-          <tr>
-            <td className="baybayin-letter">d</td>
-            <td>Da</td>
-            <td>/da/</td>
-          </tr>
-          <tr>
-            <td className="baybayin-letter">p</td>
-            <td>Fa / Pa / Pha</td>
-            <td>/fa/ /pa/</td>
-          </tr>
-          <tr>
-            <td className="baybayin-letter">h</td>
-            <td>Ha</td>
-            <td>/ha/</td>
-          </tr>
-          <tr>
-            <td className="baybayin-letter">g</td>
-            <td>Ga</td>
-            <td>/ga/</td>
-          </tr>
-          <tr>
-            <td className="baybayin-letter">l</td>
-            <td>La</td>
-            <td>/la/</td>
-          </tr>
-          <tr>
-            <td className="baybayin-letter">m</td>
-            <td>Ma</td>
-            <td>/ma/</td>
-          </tr>
-          <tr>
-            <td className="baybayin-letter">n</td>
-            <td>Na</td>
-            <td>/na/</td>
-          </tr>
-          <tr>
-            <td className="baybayin-letter">N</td>
-            <td>Nga</td>
-            <td>/ŋa/</td>
-          </tr>
-          <tr>
-            <td className="baybayin-letter">s</td>
-            <td>Sa / Za</td>
-            <td>/sa/ /za/</td>
-          </tr>
-          <tr>
-            <td className="baybayin-letter">t</td>
-            <td>Ta</td>
-            <td>/ta/</td>
-          </tr>
-          <tr>
-            <td className="baybayin-letter">w</td>
-            <td>Wa</td>
-            <td>/wa/</td>
-          </tr>
-          <tr>
-            <td className="baybayin-letter">y</td>
-            <td>Ya</td>
-            <td>/ja/</td>
-          </tr>
-          <tr>
-            <td className="baybayin-letter">i</td>
-            <td>E / I</td>
-            <td>/e/ /i/</td>
-          </tr>
-          <tr>
-            <td className="baybayin-letter">o</td>
-            <td>O / U</td>
-            <td>/o/ /u/</td>
-          </tr>
-          <tr>
-            <td className="baybayin-letter">+</td>
-            <td>Consonant marker</td>
-            <td></td>
-          </tr>
+          {BAYBAYIN_DATA.map((letter) => (
+            <tr key={letter.symbol}>
+              <td className="baybayin-letter">{letter.symbol}</td>
+              <td>{letter.letter}</td>
+              <td>{letter.sound}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </>
