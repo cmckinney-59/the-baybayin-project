1058c983e9eb7646610aa3a6bb15bb69e53ebf9d
cmckinney-59 <chazdmckinney@gmail.com>
2026-05-05 09:50:53 -0600

    implimented how to read table in Deseret

--- src/components/HowToRead/DeseretHTR/DeseretTable/DeseretTable.tsx
+++ src/components/HowToRead/DeseretHTR/DeseretTable/DeseretTable.tsx
@@ -1,152 +1,27 @@
+import { DESERET_DATA } from "../../../../data/DESERET_DATA";
+
 export default function DeseretTable() {
   return (
     <>
-      <h3>Deseret Alphabet Basic Letters</h3>
+      <h3>Deseret Alphabet Letters</h3>
       <table className="alphabet-table">
         <thead>
           <tr>
             <th>Letter</th>
             <th>IPA</th>
-            <th>Sound</th>
             <th>Name</th>
             <th>Example</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className="deseret-letter">𐐀</td>
-            <td>/i/</td>
-            <td>ee</td>
-            <td>Long I</td>
-            <td>
-              <i>e</i>at
-            </td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">B</td>
-            <td>B</td>
-            <td>Besh</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">C</td>
-            <td>C</td>
-            <td>Cresh</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">D</td>
-            <td>D</td>
-            <td>Dorn</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">E</td>
-            <td>E</td>
-            <td>Esk</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">F</td>
-            <td>F</td>
-            <td>Forn</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">G</td>
-            <td>G</td>
-            <td>Grek</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">H</td>
-            <td>H</td>
-            <td>Herf</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">I</td>
-            <td>I</td>
-            <td>Isk</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">J</td>
-            <td>J</td>
-            <td>Jenth</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">K</td>
-            <td>K</td>
-            <td>Krill</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">L</td>
-            <td>L</td>
-            <td>Leth</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">M</td>
-            <td>M</td>
-            <td>Mern</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">N</td>
-            <td>N</td>
-            <td>Nern</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">O</td>
-            <td>O</td>
-            <td>Osk</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">P</td>
-            <td>P</td>
-            <td>Peth</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">Q</td>
-            <td>Q</td>
-            <td>Qek</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">i</td>
-            <td>R</td>
-            <td>Resh</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">S</td>
-            <td>S</td>
-            <td>Senth</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">T</td>
-            <td>T</td>
-            <td>Trill</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">U</td>
-            <td>U</td>
-            <td>Usk</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">V</td>
-            <td>V</td>
-            <td>Vev</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">W</td>
-            <td>W</td>
-            <td>Wesk</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">X</td>
-            <td>X</td>
-            <td>Xesh</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">Y</td>
-            <td>Y</td>
-            <td>Yirt</td>
-          </tr>
-          <tr>
-            <td className="deseret-letter">Z</td>
-            <td>Z</td>
-            <td>Zerek</td>
-          </tr>
+          {DESERET_DATA.map((letter) => (
+            <tr key={letter.letter}>
+              <td className="deseret-letter">{letter.letter}</td>
+              <td>{letter.sound}</td>
+              <td>{letter.name}</td>
+              <td>{letter.example}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </>
