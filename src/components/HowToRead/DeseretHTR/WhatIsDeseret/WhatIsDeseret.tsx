c696d15cb17902e6a7c47aa1461c40a423e96cd1
cmckinney-59 <chazdmckinney@gmail.com>
2026-05-06 09:38:57 -0600

    updated what is Deseret

--- src/components/HowToRead/DeseretHTR/WhatIsDeseret/WhatIsDeseret.tsx
+++ src/components/HowToRead/DeseretHTR/WhatIsDeseret/WhatIsDeseret.tsx
@@ -3,8 +3,22 @@ import CollapsibleSection from "../../../CollapsibleSection/CollapsibleSection";
 export default function WhatIsDeseret() {
   return (
     <CollapsibleSection title="What Is Deseret" defaultExpanded={false}>
-      <p>Deseret is a writing system created by Brigham Young.</p>
-      <p>To explore Deseret further, read the "How To Read" section.</p>
+      <p>
+        The Deseret Alphabet was commisioned by Brigham Young to be created by
+        The University of Deseret (now known as The University of Utah).{" "}
+      </p>
+      <p>
+        To explore Deseret further, read the "How To Read" section or follow the
+        link below.
+      </p>
+      <a
+        href="https://www.churchofjesuschrist.org/study/history/topics/deseret-alphabet?lang=eng"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="link"
+      >
+        Deseret Alphabet - Church of Jesus Christ of Latter-day Saints
+      </a>
     </CollapsibleSection>
   );
 }
