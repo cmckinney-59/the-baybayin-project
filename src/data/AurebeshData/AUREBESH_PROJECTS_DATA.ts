955e0b09fe1b1db5c0295a154097985a888ed580
cmckinney-59 <chazdmckinney@gmail.com>
2026-06-16 15:41:41 -0600

    added the family parallel aurebesh

--- src/data/AurebeshData/AUREBESH_PROJECTS_DATA.ts
+++ src/data/AurebeshData/AUREBESH_PROJECTS_DATA.ts
@@ -1,6 +1,8 @@
 import type { Project } from "../../models/models";
 import theFamilyCanonPdf from "../../assets/projects/Aurebesh_TheFamily/TheFamily_Aurebesh_Canon.pdf";
+import theFamilyCanonParallelPdf from "../../assets/projects/Aurebesh_TheFamily/TheFamily_Aurebesh_Canon_Parallel.pdf";
 import theFamilyLegendsPdf from "../../assets/projects/Aurebesh_TheFamily/TheFamily_Aurebesh_Legends.pdf";
+import theFamilyLegendsParallelPdf from "../../assets/projects/Aurebesh_TheFamily/TheFamily_Aurebesh_Legends_Parallel.pdf";
 
 export const PROJECTS_DATA: Project[] = [
   {
@@ -12,10 +14,26 @@ export const PROJECTS_DATA: Project[] = [
     description: "The Family Proclamation in Canon Aurebesh.",
   },
   {
+    name: "The Family Parallel (Canon)",
+    status: "Downloadable",
+    fileUrl: theFamilyCanonParallelPdf,
+    fileName: "TheFamily_Aurebesh_Canon_Parallel.pdf",
+    fileType: "pdf",
+    description: "The Family Proclamation in Canon Aurebesh.",
+  },
+  {
     name: "The Family (Legends)",
     status: "Downloadable",
     fileUrl: theFamilyLegendsPdf,
     fileName: "TheFamily_Aurebesh_Legends.pdf",
+    fileType: "pdf",
+    description: "The Family Proclamation in Legends Aurebesh.",
+  },
+  {
+    name: "The Family Parallel (Legends)",
+    status: "Downloadable",
+    fileUrl: theFamilyLegendsParallelPdf,
+    fileName: "TheFamily_Aurebesh_Legends_Parallel.pdf",
     fileType: "pdf",
     description: "The Family Proclamation in Legends Aurebesh.",
   },
