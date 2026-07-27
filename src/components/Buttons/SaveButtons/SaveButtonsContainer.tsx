d0938891e304e0defcf2a373564ec110b15128f2
cmckinney-59 <chazdmckinney@gmail.com>
2026-07-27 00:38:55 -0600

    removed show font instalation dialog from save actions

--- src/components/Buttons/SaveButtons/SaveButtonsContainer.tsx
+++ src/components/Buttons/SaveButtons/SaveButtonsContainer.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import CopyTextButton from "./CopyTextButton";
 import ExcelSaveButton from "./ExcelSaveButton";
 import WordSaveButton from "./WordSaveButton";
-import FontInstallationDialog from "../../Dialog/FontInstallationDialog";
 import MessageDialog from "../../Dialog/MessageDialog";
 import WordSaveButtonParallel from "./WordSaveButtonParallel";
 
@@ -15,27 +14,11 @@ export default function SaveButtonContainter({
   originalText,
   transliteratedText,
 }: SaveButtonContainerProps) {
-  const [showFontDialog, setShowFontDialog] = useState(false);
   const [showMessageDialog, setShowMessageDialog] = useState(false);
   const [message, setMessage] = useState("");
-  const [pendingAction, setPendingAction] = useState<(() => void) | null>(null);
 
   const handleShowDialog = (action: () => void) => {
-    setPendingAction(() => action);
-    setShowFontDialog(true);
-  };
-
-  const handleConfirm = () => {
-    if (pendingAction) {
-      pendingAction();
-    }
-    setShowFontDialog(false);
-    setPendingAction(null);
-  };
-
-  const handleClose = () => {
-    setShowFontDialog(false);
-    setPendingAction(null);
+    action();
   };
 
   const handleMessageDialogClose = () => {
@@ -63,12 +46,6 @@ export default function SaveButtonContainter({
         transliteratedText={transliteratedText}
         onShowDialog={handleShowDialog}
       />
-      {showFontDialog && (
-        <FontInstallationDialog
-          onClose={handleClose}
-          onConfirm={handleConfirm}
-        />
-      )}
       {showMessageDialog && (
         <MessageDialog message={message} onClose={handleMessageDialogClose} />
       )}
