69d95e03d66f40179f781b17c739d862e51e9d1b
cmckinney-59 <chazdmckinney@gmail.com>
2026-09-04 07:19:29 -0600

    filled out What Is Aurebesh secton

--- src/components/HowToRead/AurebeshHTR/WhatIsAurebesh.tsx
+++ src/components/HowToRead/AurebeshHTR/WhatIsAurebesh.tsx
@@ -3,8 +3,20 @@ import CollapsibleSection from "../../CollapsibleSection/CollapsibleSection";
 export default function WhatIsAurebesh() {
   return (
     <CollapsibleSection title="What Is Aurebesh" defaultExpanded={false}>
-      <p>Aurebesh is a fictional alphabet used in the Star Wars franchise.</p>
-      <p>To explore Aurebesh further, see the "How To Read" and "More Resources" sections.</p>
+      <p>
+        Aurebesh is a fictional alphabet used in the Star Wars franchise. The
+        characters first appeared in Return of the Jedi, in 1983. At the time
+        they were just symbols displayed on a screen and had no meaning. In
+        1993, with the approval of Lucasfilm, a table top game creator used
+        those symbols and invented new ones mapping them to letters in the Latin
+        alphabet. Since then, Aurebesh has been used in many Star Wars related
+        products, including books, games, movies and even props throughout
+        Disneyland.
+      </p>
+      <p>
+        To explore Aurebesh further, see the "How To Read" and "More Resources"
+        sections.
+      </p>
     </CollapsibleSection>
   );
 }
