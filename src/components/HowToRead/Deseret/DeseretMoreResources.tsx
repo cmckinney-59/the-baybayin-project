eaed4681355ceec55aa879f6e8285a274380cf85
cmckinney-59 <chazdmckinney@gmail.com>
2026-09-12 13:11:25 -0600

    work on Deseret resources

--- src/components/HowToRead/Deseret/DeseretMoreResources.tsx
+++ src/components/HowToRead/Deseret/DeseretMoreResources.tsx
@@ -5,12 +5,20 @@ export default function DeseretMoreResources() {
   return (
     <CollapsibleSection title="More Resources" defaultExpanded={false}>
       <p>
-        Here are some more resources for learning about the different writing
-        systems.
+        Here are some more resources for learning about The Deseret Alphabet.
       </p>
       <ul className="resource-links">
         <ResourceLink href="https://www.deseretalphabet.org/">
           Deseret Alphabet
+        </ResourceLink>
+        <ResourceLink href="https://www.2deseret.com/">
+          Deseret Translator
+        </ResourceLink>
+        <ResourceLink href="https://www.churchofjesuschrist.org/study/history/topics/deseret-alphabet?lang=eng">
+          LDS Church Deseret Alphabet
+        </ResourceLink>
+        <ResourceLink href="https://restorethealphabet.com">
+          Restore The Alphabet
         </ResourceLink>
       </ul>
     </CollapsibleSection>
